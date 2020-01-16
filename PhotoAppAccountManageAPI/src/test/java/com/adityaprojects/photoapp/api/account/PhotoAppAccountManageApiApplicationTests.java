7ceb3b4fd4454e4e9bad8cbf97c5c93d2728e7b9
adityakulai07 <adityakulai93@gmail.com>
2020-01-15 16:02:18 -0800

    First commit

---
+++ PhotoAppAccountManageAPI/src/test/java/com/adityaprojects/photoapp/api/account/PhotoAppAccountManageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityaprojects.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppAccountManageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PhotoAppEurekaDiscovery/src/test/java/com/adityaprojects/photoapp/discovery/PhotoAppEurekaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityaprojects.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppEurekaDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
